c415d8f05a2f65829966981701bc91495ce76904
ForMyke <golomian16@hotmail.com>
2026-05-21 22:53:26 -0600

    feat: files

---
+++ Servicio/servicio/Articulo.java
@@ -0,0 +1,11 @@
+package servicio;
+
+public class Articulo
+{
+    public Integer id_articulo;
+    public String nombre;
+    public String descripcion;
+    public Double precio;
+    public Integer cantidad;
+    public byte[] foto;
+}
